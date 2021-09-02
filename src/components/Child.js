5780503ded097cdc1749d7796229dbae42bb6055
MoonJuhan <codemike@naver.com>
2021-09-02 17:21:57 +0900

    feat: State Hook 관련 학습

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
 const Child = (props) => {
+  console.log('Created and Updated')
+
+  const [count, setCount] = useState(0)
+
+  const onClickButton = () => {
+    console.log('Click')
+    setCount(count + 1)
+  }
+
   return (
     <div>
       <h2>Child : {props.name}</h2>
+      <h3>Count : {count}</h3>
+      <button onClick={onClickButton}>Button</button>
     </div>
   )
 }
